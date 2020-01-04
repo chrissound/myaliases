46685d9686f34b56443cd671eb009dd8ded5ee1a
Chris S <chris@soundshare.co.uk>
2020-01-04 19:19:52 +0000

    moscovium history use json instead as it's faster, new H binding for recursive

--- chris-misc.plugin.zsh
+++ chris-misc.plugin.zsh
@@ -10,8 +10,14 @@ alias mf="touch"
 
 alias vim="nvim"
 alias emacsp="emacsclient -t"
+
 alias l="ll"
 alias ll="exa -l --group-directories-first --sort=extension"
+alias lln="exa -l --group-directories-first --sort=name"
+alias llnr="exa -l --group-directories-first --sort=name --reverse"
+alias lt="exa -l --group-directories-first --sort=modified"
+alias ltr="exa -l --group-directories-first --sort=modified --reverse"
+
 alias r='ranger --choosedir=/tmp/.rangerdir; LASTDIR=`cat /tmp/.rangerdir`; cd "$LASTDIR"'
 alias grep="rg"
 alias docker="sudo docker"
@@ -22,6 +28,7 @@ alias ptet="$EDITOR tasks.txt"
 alias ptel="$EDITOR log.json"
 alias ptll="pitanga log | less -R"
 alias ptl="pitanga log --first=3 "
+alias ptrrr="pitanga amend --restart"
 pts () {
   echo "$#"
   if [[ "$#" = 0 ]]; then
@@ -34,17 +41,25 @@ alias ptsp="pitanga stop"
 ptr () { pitanga resume --accept-suggestion="$(pitanga resume --suggestions | tac | fzf)" }
 alias ptrl="pitanga resume" 
 
-beepboop () {
-  if [ -z "$IGNORE_HIST_CHRIS" ] || return 0
-  echo -n "$1" >> .directory_history
+openFzfDirectoryHistoryNonRecursive() {
+    RBUFFER=$(moscoviumorange --path=$PWD --limit 999999 --json |  jq -r '.[] | .[1]' | tac | \
+	                fzf --height 40% \
+--bind 'ctrl-y:execute-silent(echo -n {} | xclip)+abort'
+    )
+
+    zle redisplay
+	  zle end-of-line;
+	  zle accept-line;
 }
 
+zle     -N   openFzfDirectoryHistoryNonRecursive
+bindkey '^P' openFzfDirectoryHistoryNonRecursive
+
 openFzfDirectoryHistory() {
-  # Sort by frequency
-  RBUFFER=$(cat .directory_history | sort | uniq -c | sort -rn | sed -e 's/\s*[0-9]*\s*//' | \
-	  fzf --height 40% \
-           	--bind 'ctrl-y:execute-silent(echo -n {} | xclip)+abort'
-  )
+    RBUFFER=$(moscoviumorange --path-prefix=$PWD --limit 999999 --json |  jq -r '.[] | .[1]' | tac | \
+	            fzf --height 40% \
+                     	--bind 'ctrl-y:execute-silent(echo -n {} | xclip)+abort'
+    )
 
   zle redisplay
 	zle end-of-line;
@@ -52,8 +67,7 @@ openFzfDirectoryHistory() {
 }
 
 zle     -N   openFzfDirectoryHistory
-bindkey '^P' openFzfDirectoryHistory
-add-zsh-hook zshaddhistory beepboop
+bindkey '^H' openFzfDirectoryHistory
 
 openFzfDirectoryHistoryUnsorted() {
   # Sort by frequency
@@ -80,15 +94,21 @@ zle     -N   cdUpAbcxyz
 bindkey '^K' cdUpAbcxyz
 
 __git_files () { 
-    _wanted files expl 'local files' _files     
+    _wanted files expl 'local files' _files
 }
 
 HISTSIZE=1000000
 
 function printc () {
-  (nohup ~/ScriptsVcs/addEntryToMoscoviumOrange.sh "$1" &) > /dev/null 2>&1
+  if [ -S ~/.config/moscoviumOrange/monitor.soc ]; then
+      $(jq -n --arg command "$1" --arg path "$PWD" '{"command":$command, "path":$path}' | "$(echo 'readlink -f $(which nc)' | nix run nixpkgs.netcat)" -N -U ~/.config/moscoviumOrange/monitor.soc &)
+  else
+      echo "Failed to log command to moscoviumorange" > /dev/stderr
+  fi
 }
 
 autoload -Uz  add-zsh-hook
 
 add-zsh-hook preexec printc
+
+function wrl () { readlink -f "$(which $1)" ; }
